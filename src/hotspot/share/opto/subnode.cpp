f98a58966bdfbe8af9ab8e26295b28e85e6725bb
Zhiqiang Zang <zhiqiang.zang@utexas.edu>
2022-04-16 17:34:45 -0500

    replace manual code with generated code for pSub11.

--- src/hotspot/share/opto/subnode.cpp
+++ src/hotspot/share/opto/subnode.cpp
@@ -274,11 +274,20 @@ Node *SubINode::Ideal(PhaseGVN *phase, bool can_reshape){
     return new SubINode( in1->in(1), in2->in(1) );
   /* pSub10 END */
 
-  /* pSub11 START */
-  // Convert "(A+X) - (X+B)" into "A - B"
-  if( op1 == Op_AddI && op2 == Op_AddI && in1->in(2) == in2->in(1) )
-    return new SubINode( in1->in(1), in2->in(2) );
-  /* pSub11 END */
+{
+Node* _JOG_in1 = in(1);
+Node* _JOG_in11 = _JOG_in1 != NULL && 1 < _JOG_in1->req() ? _JOG_in1->in(1) : NULL;
+Node* _JOG_in12 = _JOG_in1 != NULL && 2 < _JOG_in1->req() ? _JOG_in1->in(2) : NULL;
+Node* _JOG_in2 = in(2);
+Node* _JOG_in21 = _JOG_in2 != NULL && 1 < _JOG_in2->req() ? _JOG_in2->in(1) : NULL;
+Node* _JOG_in22 = _JOG_in2 != NULL && 2 < _JOG_in2->req() ? _JOG_in2->in(2) : NULL;
+if (_JOG_in1->Opcode() == Op_AddI
+    && _JOG_in2->Opcode() == Op_AddI
+    && _JOG_in12 == _JOG_in21) {
+return new SubINode(_JOG_in11, _JOG_in22);
+}
+}
+
 
   /* pSub12 START */
   // Convert "(X+A) - (B+X)" into "A - B"
